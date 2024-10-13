32f40346d6cf1e5e264ea98f24a6cce5c58ce510
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-10-13 22:37:05 +0530

    added

---
+++ index.js
@@ -0,0 +1,52 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather' ;
+const apiKey = '13c4bf8c9a2e4edad2c63dd362f4ade4' ;
+
+window.onload = function() {
+    weatherFn("Pune") ;
+}
+
+async function weatherFn() {
+
+    const cName = document.getElementById('city-input').value;
+
+    const temp = `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const response = await fetch(temp) ;
+        const data = await response.json() ; 
+        if(response.ok) {
+            console.log(data);
+            weatherShowfn(data);
+        }
+        else{
+            alert('city not Found. Please try again') ;
+        }
+
+    } 
+
+    catch(error){
+        console.error('Error Fetching Weather Data : ', error) ;
+    }
+
+}
+
+
+
+
+
+function weatherShowfn(data) {
+
+    document.getElementById('city-name').textContent = data.name ;
+
+    document.getElementById('date').textContent = moment().format('MMMM Do YYYY , h:mm:ss a') ;
+
+    document.getElementById('temperature').innerHTML = `${data.main.temp} C` ;
+   
+    document.getElementById('description').textContent = data.weather[0].description;
+
+    document.getElementById('wind-speed').innerHTML = `Wind Speed: ${data.wind.speed}` ; 
+
+    document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    document.getElementById('weatherINFO').style.display = 'block';
+
+}
