f79d7af5ad15716dea621dc17957d9d077910efd
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-10-16 21:18:48 +0530

    feat: Also Added next 4 days

--- index.js
+++ index.js
@@ -89,4 +89,10 @@ function weatherShowfn(data,dailyforcast) {
 
     document.getElementById("day1").innerHTML = `${dailyforcast[0].main.temp}`;
 
+    document.getElementById("day2").innerHTML = `${dailyforcast[1].main.temp}`;
+
+    document.getElementById("day3").innerHTML = `${dailyforcast[2].main.temp}` ;
+
+    document.getElementById("day4").innerHTML = `${dailyforcast[3].main.temp}` ;
+
 }
