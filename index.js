81397dd0449a923310119380d98f366720c36385
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-10-21 21:05:02 +0530

    feat: successfully added dayname foreach forcast temperature

--- index.js
+++ index.js
@@ -88,12 +88,11 @@ function weatherShowfn(data,dailyforcast) {
 
     document.getElementById('weatherINFO').style.display = 'block';
 
-    document.getElementById("day1").innerHTML = `${Math.floor(dailyforcast[0].main.temp)}°`;
+    document.getElementById("day1").innerHTML = `${moment(dailyforcast[0].dt_txt).format('dddd')}: ${Math.floor(dailyforcast[0].main.temp)}°`;
 
-    document.getElementById("day2").innerHTML = `${Math.floor(dailyforcast[1].main.temp)}°`;
+    document.getElementById("day2").innerHTML = `${moment(dailyforcast[1].dt_txt).format('dddd')}: ${Math.floor(dailyforcast[0].main.temp)}°`;
 
-    document.getElementById("day3").innerHTML = `${Math.floor(dailyforcast[2].main.temp)}°` ;
+    document.getElementById("day3").innerHTML = `${moment(dailyforcast[2].dt_txt).format('dddd')}: ${Math.floor(dailyforcast[0].main.temp)}°`;
 
-    document.getElementById("day4").innerHTML = `${Math.floor(dailyforcast[3].main.temp)}°` ;
-
+    document.getElementById("day4").innerHTML = `${moment(dailyforcast[3].dt_txt).format('dddd')}: ${Math.floor(dailyforcast[0].main.temp)}°`;
 }
